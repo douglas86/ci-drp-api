0d8ddf151381f2667f25a0b05af34cdd5d8dc116
douglas <douglasmaxton@gmail.com>
2024-04-08 18:12:49 +0100

    added: updating for object based on id

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 import asyncio
 from adrf.views import APIView
 from django.http import Http404
+from rest_framework import status
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -8,6 +9,7 @@ from .serializers import ProfileSerializer
 
 # example of async generator and async coroutine
 # https://superfastpython.com/asyncio-async-for/
+
 
 class ProfileList(APIView):
     model = Profile
@@ -28,10 +30,11 @@ class ProfileList(APIView):
 
 class ProfileDetail(APIView):
     model = Profile
+    serializer_class = ProfileSerializer
 
     def get_object(self, pk):
         try:
-            profile = Profile.objects.get(pk=pk)
+            profile = self.model.objects.get(pk=pk)
             return profile
         except Profile.DoesNotExist:
             raise Http404
@@ -40,3 +43,12 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
