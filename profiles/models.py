3f1d420e80f0dbd846bee9f481fdcf5af0297ddf
douglas <douglasmaxton@gmail.com>
2024-04-08 20:04:25 +0100

    added: image_filter choices for post model

--- profiles/models.py
+++ profiles/models.py
@@ -6,12 +6,30 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Early Bird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro 11'),
+    ]
+
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/', default='../default_profile_rkmhff')
+    image_filter = models.CharField(max_length=255, choices=image_filter_choices, default='normal')
 
     class Meta:
         ordering = ['-created_at']
