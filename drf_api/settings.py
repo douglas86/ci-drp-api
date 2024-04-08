428713b7d644ae43883bdf1d12e2db5f0b8c224d
douglas <douglasmaxton@gmail.com>
2024-04-08 14:03:23 +0100

    added: media and default file storage const

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -18,6 +18,8 @@ if os.path.exists('env.py'):
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
+MEDIA = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
