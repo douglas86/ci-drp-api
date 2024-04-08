4e6a62d6851a89326a397b49516f11ae7b46dca6
douglas <douglasmaxton@gmail.com>
2024-04-08 17:12:17 +0100

    created: async functions for model

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+import asyncio
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -5,7 +6,17 @@ from .serializers import ProfileSerializer
 
 
 class ProfileList(APIView):
+    model = Profile
+
+    async def async_generator(self):
+        profiles = self.model.objects.all()
+        yield profiles
+
+    async def async_coroutine(self):
+        async for profile in self.async_generator():
+            return profile
+
     def get(self, request):
-        profiles = Profile.objects.all()
+        profiles = asyncio.run(self.async_coroutine())
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
