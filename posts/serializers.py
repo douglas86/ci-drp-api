523ff2d76d09f042a271c87bcfc28d8896944db2
douglas <douglasmaxton@gmail.com>
2024-04-08 19:58:01 +0100

    rectified: post serializers content

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,8 +5,8 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.id')
-    profile_image = serializers.ReadOnlyField(source='owner.image')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -14,4 +14,5 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'owner', 'created_at', 'updated_at', 'title', 'content', 'image', ]
+        fields = ['id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at', 'updated_at', 'title',
+                  'content', 'image', 'image_filter', ]
