b3eedfb70b1b8bd9aa5750ca2a0f4eaac4f7fcd9
douglas <douglasmaxton@gmail.com>
2024-04-08 19:42:26 +0100

    created: Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/', default='default_post_r8m7an', blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
