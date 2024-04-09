a0b727d343c69ea03dc957a4377335de405e34ae
douglas <douglasmaxton@gmail.com>
2024-04-09 16:42:12 +0100

    created: like list view with perform_create method

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from likes.models import Like
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
