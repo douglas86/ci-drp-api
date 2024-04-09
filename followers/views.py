362ddde87cc224842b043959a5df8d9d9bfe11d1
douglas <douglasmaxton@gmail.com>
2024-04-09 17:22:29 +0100

    created: FollowerListAPIView

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from followers.models import Follower
+from drf_api.permissions import IsOwnerOrReadOnly
+from followers.serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerListAPIView(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
