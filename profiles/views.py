193b9872ae6d2bbeae92606bff90841ce9478147
douglas <douglasmaxton@gmail.com>
2024-04-10 13:35:15 +0100

    added: filterset_fields to profile filters

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,7 @@ from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # example of async generator and async coroutine
@@ -23,7 +24,8 @@ class ProfileList(generics.ListAPIView):
         following_count=Count('owner__following', distinct=True),
     )
     serializer_class = ProfileSerializer
-    filter_backends = (filters.OrderingFilter,)
+    filter_backends = (filters.OrderingFilter, DjangoFilterBackend)
+    filterset_fields = ['owner__following__followed__profile']
     ordering_fields = ['posts_count', 'followers_count', 'following_count', 'owner__following__created_at',
                        'owner__followed__created_at']
 
