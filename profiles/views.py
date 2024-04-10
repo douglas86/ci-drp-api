6da6b862eddfefda2a218796ddc1c12bf81b25e9
douglas <douglasmaxton@gmail.com>
2024-04-10 12:51:55 +0100

    added: posts, followers and following count to profile details

--- profiles/views.py
+++ profiles/views.py
@@ -50,5 +50,9 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        follwers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    )
     serializer_class = ProfileSerializer
