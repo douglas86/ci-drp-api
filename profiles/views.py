e7ab84a00b203a7de5f86dd935b52248e07c309a
douglas <douglasmaxton@gmail.com>
2024-04-10 13:54:42 +0100

    added: filter for owner__followed__owner__profile

--- profiles/views.py
+++ profiles/views.py
@@ -25,7 +25,7 @@ class ProfileList(generics.ListAPIView):
     )
     serializer_class = ProfileSerializer
     filter_backends = (filters.OrderingFilter, DjangoFilterBackend)
-    filterset_fields = ['owner__following__followed__profile']
+    filterset_fields = ['owner__following__followed__profile', 'owner__followed__owner__profile']
     ordering_fields = ['posts_count', 'followers_count', 'following_count', 'owner__following__created_at',
                        'owner__followed__created_at']
 
